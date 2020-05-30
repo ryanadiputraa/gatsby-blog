43ef2d7e9a02b98a8f2165637fed902d3b504efd
Ryan AdiPutra <ryannadiputraa@gmail.com>
2020-05-30 10:08:26 +0800

    created tag posts page

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,8 @@ exports.createPages = ({ actions, graphql }) => {
 
   const templates = {
     singlePost: path.resolve('src/templates/single-post.js'),
-    tagsPage: path.resolve('src/templates/tags-page.js')
+    tagsPage: path.resolve('src/templates/tags-page.js'),
+    tagPosts: path.resolve('src/templates/tag-posts.js')
   }
 
   return graphql(`
@@ -81,6 +82,17 @@ exports.createPages = ({ actions, graphql }) => {
         tags, 
         tagPostCounts
       }
+    })
+
+    // create tag posts page
+    tags.forEach(tag => {
+      createPage({
+        path: `/tag/${slugify(tag)}`,
+        component: templates.tagPosts,
+        context: {
+          tag,
+        }
+      })
     })
   })
 }
