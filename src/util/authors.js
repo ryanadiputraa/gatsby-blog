929708641c8ffae91df794d003fe838f0db1eac3
Ryan <ryannadiputraa@gmail.com>
2020-10-16 09:14:41 +0800

    edit about section

--- src/util/authors.js
+++ src/util/authors.js
@@ -2,7 +2,7 @@ const authors = [
   {
     name: 'Ryan Adi Putra',
     imageUrl: 'icon.png',
-    bio: "I'am a university student in Tadulako University majoring Information Technology. I'am a data science enthusiast because i passionate on Machine Learning, Artificial Intelligence, etc.",
+    bio: "I'am a university student in Tadulako University majoring Information Technology. I'am a data science, and Machine Learning enthusiast. You can check out my project on my github pages, feel free to contribute :)",
     facebook: 'https://www.facebook.com/ryan.adiputra.1426',
     twitter: 'https://twitter.com/RyanAdi33024016',
     instagram: 'https://www.instagram.com/bukanryan/',
